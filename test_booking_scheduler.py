a22f2396b38f36ee88d32d38d2c4aa7831b8bea6
jihye <jhlov.jin@samsung.com>
2024-06-14 11:54:33 +0900

    unittest - step9

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -65,7 +65,15 @@ class BookingSchedulerTest(unittest.TestCase):
 
         self.assertEqual(testable_mail_sender.get_count_send_mail_is_called(), 0)
     def test_이메일이_있는_경우에는_이메일_발송(self):
-        pass
+        customer_with_mail = Customer("FAKE NAME", "010-2824-3421", "test@test.com")
+        testable_mail_sender = TestMailSender()
+        schedule = Schedule(ON_THE_HOUR, CAPACITY_PER_HOUR, customer_with_mail)
+        self.booking_scheduler.set_mail_sender(testable_mail_sender)
+
+        self.booking_scheduler.add_schedule(schedule)
+
+        self.assertEqual(testable_mail_sender.get_count_send_mail_is_called(), 1)
+
 
     def test_현재날짜가_일요일인_경우_예약불가_예외처리(self):
         pass
